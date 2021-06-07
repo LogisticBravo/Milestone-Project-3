b233e55447e833c6b5804ef1464e73c70235e6d6
LogisticBravo <jay.jbradley@gmail.com>
2021-06-07 23:14:34 +0000

    Add login form to navbar. Add signup.html to templates

--- app.py
+++ app.py
@@ -29,6 +29,11 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
