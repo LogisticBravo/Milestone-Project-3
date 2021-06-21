44db3efeae5fd829fe0d3b5e389b0fcd33c50077
LogisticBravo <jay.jbradley@gmail.com>
2021-06-21 21:55:31 +0000

    Add passwords don't match defensive programming to signup.js

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,13 +1,15 @@
 document.getElementById('confirm_password').addEventListener("change", unlock);
-
+$('p').hide()
 
 function unlock() {
     if ($('#new_password').val() !== $('#confirm_password').val())
     {
     $('#submit').prop("disabled", true);
+    $('p').show()
     }
     else if ($('#new_password').val() === $('#confirm_password').val())
     {
     $('#submit').removeAttr("disabled");
+    $('p').hide()
     }
 }
