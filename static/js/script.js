791458921a41acc381e52bed2ce1a67c5c0a1e16
LogisticBravo <jay.jbradley@gmail.com>
2021-06-22 21:38:16 +0000

    Add if else jinja statement to profile for when reviews or favourites are empty

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,11 @@ var toastElList = [].slice.call(document.querySelectorAll('.toast'))
 var toastList = toastElList.map(function (toastEl) {
   return new bootstrap.Toast(toastEl)
 })
+
+$(document).ready(function() {
+    var myModal = new bootstrap.Modal(document.getElementById('writeReviewToggle'), {
+  keyboard: false
+})
+    if (window.location.hash.indexOf("writeReviewToggle") > -1) {
+      myModal.show();
+    }});
