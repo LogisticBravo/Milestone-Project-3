bb00ca0c80c9660999e34073490e5bddf408075d
LogisticBravo <jay.jbradley@gmail.com>
2021-06-22 21:47:53 +0000

    Fix bug where favourite button only shows for review creator

--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,18 @@
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 var toastElList = [].slice.call(document.querySelectorAll('.toast'))
 var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl)
+    return new bootstrap.Toast(toastEl)
 })
 
-$(document).ready(function() {
+$(document).ready(function () {
     var myModal = new bootstrap.Modal(document.getElementById('writeReviewToggle'), {
-  keyboard: false
-})
+        keyboard: false
+    })
     if (window.location.hash.indexOf("writeReviewToggle") > -1) {
-      myModal.show();
-    }});
+        myModal.show();
+    }
+});
