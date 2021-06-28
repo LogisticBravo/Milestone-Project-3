6d884d23170c95965a4575cb9f6468681f7089d3
LogisticBravo <jay.jbradley@gmail.com>
2021-06-28 22:58:28 +0000

    Fix bug where subscribe toggle would stay on it's triggered state after cancel form submit

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
-if ($("#subscription").attr("checked") == "checked") {
-    $("#subscription").attr("data-bs-target", "#unsubscribe")
-} else if ($("#subscription").attr("checked") == undefined) {
-    $("#subscription").attr("data-bs-target", "#subscribe")
-}})
+    function checked(state) {
+            $("#subscription").prop("checked", state)
+        };
+    if ($("#subscription").attr("checked") == "checked") {
+        $("#subscription").attr("data-bs-target", "#unsubscribe");
+        var closeButton = document.getElementById("unsubClose");
+        var cancelButton = document.getElementById("unsubCancel");
+        closeButton.onclick = function(){checked("true");}
+        cancelButton.onclick = function(){checked("true");}
+    } else if ($("#subscription").prop("checked") == false) {
+        $("#subscription").attr("data-bs-target", "#subscribe");
+        var subCloseButton = document.getElementById("subClose");
+        var subCancelButton = document.getElementById("subCancel");
+        subCloseButton.onclick = function(){checked(false);}
+        subCancelButton.onclick = function(){checked(false);}
+    }
+})
