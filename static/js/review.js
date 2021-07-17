1eb2775c2e297b76f35503d9274adc80c391b335
LogisticBravo <jay.jbradley@gmail.com>
2021-07-17 20:53:36 +0000

    Remove window.onload

--- static/js/review.js
+++ static/js/review.js
@@ -1,6 +1,3 @@
-window.onload = function(){
-
-
 /* triggers newsletter modal. Code partially from: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_onscroll3
 local storage code partially from https://stackoverflow.com/questions/8123032/how-do-i-make-a-count-variable-persistent-across-sessions
 takes the users email address and stores it to local storage to be poulated on the signup page if it's triggered. i.e if they are not signed in. */
@@ -264,4 +261,4 @@ $("#five-rating").click(function () {
     prevCheck("rating-4", "rating-3", "three-rating")
     prevCheck("rating-3", "rating-2", "two-rating")
     prevCheck("rating-2", "rating-1", "one-rating")
-})}}
+})}
